160400c51f5f660da1676dd2497c918c7e2d565a
Joey Etamity <joey.etamity@miceanddice.com>
2017-11-08 20:11:01 +0000

    Update

---
+++ App/Lib/Shared.js
@@ -0,0 +1,7 @@
+import ParseApp from './ParseApp';
+import { ParseConfig } from 'app/config/appConstants';
+
+const App = new ParseApp(ParseConfig);
+export default Shared = {
+    App
+}
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 import StorybookUI from './storybook'
 
-const __DEBUG__ = false
+const __DEBUG__ = true
 
 AppRegistry.registerComponent('Universal', () => __DEBUG__ ? StorybookUI : App)
